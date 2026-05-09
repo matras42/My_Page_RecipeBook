e592d19ca42bd3457db8adfde54a135198aec56f
matras42 <matras42@users.noreply.github.com>
2026-05-09 21:30:21 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+</main>
+<footer class="footer">
+    <div class="wrap footer-inner">
+        <span>Кулінарна книга</span>
+        <span>Палій Вадим, група П-31</span>
+    </div>
+</footer>
+</body>
+</html>
