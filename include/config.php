4abd8b9acc10f7baf3f0b70b5152371028cacef3
matras42 <matras42@users.noreply.github.com>
2026-05-09 21:30:22 +0300

    Create config.php

---
+++ include/config.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'recipe_book');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('JSON_STORAGE', __DIR__ . '/../data/storage.json');
+?>
